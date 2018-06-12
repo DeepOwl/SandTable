f9a4b827f542075bbf4c95b48f150d29da61a11a
Daniel Powell <danieljtp@gmail.com>
2018-06-11 20:38:34 -0400

    editable entity fields
    they don't push to db yet

--- src/app/campaign-list/campaign-list.component.ts
+++ src/app/campaign-list/campaign-list.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CampaignService } from '../_services/campaign.service';
 import { Observable } from 'rxjs/Observable';
 
-export interface Campaign { name:string; description:string; owner:string; }
-export interface CampaignId extends Campaign { id: string; }
+
 
 @Component({
   selector: 'app-campaign-list',
